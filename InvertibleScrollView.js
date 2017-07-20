32c8a553605bf97b58e18e2c54c0d9f0bc6b803c
Julius Kabangi <kabangijulius@gmail.com>
2017-07-20 21:30:42 +0300

    update proptypes

--- InvertibleScrollView.js
+++ InvertibleScrollView.js
@@ -1,18 +1,14 @@
 'use strict';
 
-import {PropTypes} from 'react';
+import PropTypes from 'prop-types';
 import createReactClass from 'create-react-class';
-import {
-  ScrollView,
-  StyleSheet,
-  View,
-} from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
 import ScrollableMixin from 'react-native-scrollable-mixin';
 
 import cloneReferencedElement from 'react-clone-referenced-element';
 
 type DefaultProps = {
-  renderScrollComponent: (props: Object) => ReactElement;
+  renderScrollComponent: (props: Object) => ReactElement
 };
 
 let InvertibleScrollView = createReactClass({
@@ -22,12 +18,12 @@ let InvertibleScrollView = createReactClass({
   propTypes: {
     ...ScrollView.propTypes,
     inverted: PropTypes.bool,
-    renderScrollComponent: PropTypes.func.isRequired,
+    renderScrollComponent: PropTypes.func.isRequired
   },
 
   getDefaultProps(): DefaultProps {
     return {
-      renderScrollComponent: props => <ScrollView {...props} />,
+      renderScrollComponent: props => <ScrollView {...props} />
     };
   },
 
@@ -40,24 +36,28 @@ let InvertibleScrollView = createReactClass({
   },
 
   render() {
-    var {
-      inverted,
-      renderScrollComponent,
-      ...props,
-    } = this.props;
+    var { inverted, renderScrollComponent, ...props } = this.props;
 
     if (inverted) {
       if (this.props.horizontal) {
         props.style = [styles.horizontallyInverted, props.style];
-        props.children = this._renderInvertedChildren(props.children, styles.horizontallyInverted);
+        props.children = this._renderInvertedChildren(
+          props.children,
+          styles.horizontallyInverted
+        );
       } else {
         props.style = [styles.verticallyInverted, props.style];
-        props.children = this._renderInvertedChildren(props.children, styles.verticallyInverted);
+        props.children = this._renderInvertedChildren(
+          props.children,
+          styles.verticallyInverted
+        );
       }
     }
 
     return cloneReferencedElement(renderScrollComponent(props), {
-      ref: component => { this._scrollComponent = component; },
+      ref: component => {
+        this._scrollComponent = component;
+      }
     });
   },
 
@@ -65,22 +65,18 @@ let InvertibleScrollView = createReactClass({
     return React.Children.map(children, child => {
       return child ? <View style={inversionStyle}>{child}</View> : child;
     });
-  },
+  }
 });
 
 let styles = StyleSheet.create({
   verticallyInverted: {
     flex: 1,
-    transform: [
-      { scaleY: -1 },
-    ],
+    transform: [{ scaleY: -1 }]
   },
   horizontallyInverted: {
     flex: 1,
-    transform: [
-      { scaleX: -1 },
-    ],
-  },
+    transform: [{ scaleX: -1 }]
+  }
 });
 
 module.exports = InvertibleScrollView;
